c92e8b3e553bc5f471d4c2509230d2a5c0d8feec
daadam4 <19582823+daadam4@users.noreply.github.com>
2023-01-22 19:51:04 -0500

    added comments

--- int_conf.py
+++ int_conf.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
+
+# import the pyeapi client which is used to communicated with the eAPI
 import pyeapi
 
-# Connect to the Arista EOS device
+# Connect to the Arista EOS device using the function connect within the client module
+# For more info on pyeapi checkout the docs @ https://pyeapi.readthedocs.io/en/master/client_modules/client.html
 node = pyeapi.client.connect(host='192.168.0.102', username='arista', password='password', transport='https', return_node=True)
 
-# Configure interface Ethernet1
+# Create a list of commands and store it in the variable "commands"
+# The variable name "commands" is arbitrary and could be x
+# "commands" was used for clarity
 commands = [
     'interface Ethernet1',
     'description Configured by Arista Python script',
     'no shutdown',
 ]
 
+# Usings the config function to pass the commands to the node that we have connected to
+# the commands that are being passed are in the list above
 node.config(commands)
 
+
+# At this point Ethernet1 has been configured with the description we passed in
+# The following validation checks are in place for the demo and to ensure it worked as expected
+# The variable validate here is the same concept as "commands" above
 validate = [
     'show running-config interfaces Ethernet1'
 ]
 
+# We store the output of the validate configurations to "result"
 result = node.run_commands(validate, encoding='text')
 
+# Run a for loop to print the output
 for element in result:
     for _, m in element.items():
         print(f"command:\n{validate[0]}\nresult:\n{m}")
--- int_conf_multi.py
+++ int_conf_multi.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
+
+# This script builds on the int_conf.py script by adding multiple hosts and
+# Multiple interfaces. the comments there provide building blocks for this code
 import pyeapi
 
+# Create a list of hosts which we will loop through
 hosts = ["192.168.0.101", "192.168.0.102"]
 
+# For every item in the hosts list above execute every subsequent line
 for ip_address in hosts:
     
+    # Connect to the EOS device
     node = pyeapi.client.connect(host=ip_address, username='arista', password='password', transport='https', return_node=True)
 
+    # For every number in the range, do the following
     for interface in range(1,49):
-        # Configure interface Ethernet1
+        
+        # Same code as int_conf.py except we use a variable input for the interface
         commands = [
         f'interface Ethernet{interface}',
         'description Configured by Arista Python script',
@@ -17,6 +25,7 @@ for ip_address in hosts:
 
         node.config(commands)
 
+    # All the same as int_conf.py
     validate = [
         f'show running-config interfaces Ethernet1'
     ]
